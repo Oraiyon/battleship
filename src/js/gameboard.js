83d2f1e0147b42b7890f62bd78c479eac3af3eb1
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-11-02 14:09:46 +0800

    Created and tested createShips() Creates the 5 ships for both player and enemy

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -8,7 +8,9 @@ export class Gameboard {
     this.missedShots = [];
     this.playerShips = [];
     this.enemyShips = [];
-    this.sunkenShips = [];
+    this.placedPlayerShips = [];
+    this.sunkenPlayerShips = [];
+    this.sunkenEnemyShips = [];
   }
 
   createGameboard() {
@@ -16,6 +18,31 @@ export class Gameboard {
       for (let i = 0; i < 10; i++) {
         this.board.push(["", "", "", "", "", "", "", "", "", ""]);
       }
+      this.createShips();
+    }
+  }
+
+  createShips() {
+    if (this.playerShips.length === 0 && this.enemyShips.length === 0) {
+      const carrier = new Ship("Carrier", 5);
+      const battleship = new Ship("Battleship", 4);
+      const destroyer = new Ship("Destroyer", 3);
+      const submarine = new Ship("Submarine", 3);
+      const patrolBoat = new Ship("PatrolBoat", 2);
+      this.playerShips.push(
+        carrier,
+        battleship,
+        destroyer,
+        submarine,
+        patrolBoat,
+      );
+      this.enemyShips.push(
+        carrier,
+        battleship,
+        destroyer,
+        submarine,
+        patrolBoat,
+      );
     }
   }
 }
--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -6,8 +6,14 @@ describe("Tests for Gameboard properties", () => {
     gameBoard = new Gameboard();
   });
 
-  test("Checks if Gameboard is created", () => {
+  test("Checks if Gameboard is created only once", () => {
     gameBoard.createGameboard();
     expect(gameBoard.board.length).toBe(10);
+  });
+
+  test("Checks if ships are created for player ", () => {
+    gameBoard.createGameboard();
+    expect(gameBoard.playerShips.length).toBe(5);
+    expect(gameBoard.enemyShips.length).toBe(5);
   });
 });
